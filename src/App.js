a160e7b96a388601938ab5366f98abdf7a39d68c
Ashwinth <66359270+Ashwinth-Git@users.noreply.github.com>
2022-03-30 15:20:16 +0530

    Added the source folder and the main files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import GlobalStyles from "./styles/GlobalStyles"
+import { ThemeProvider } from 'styled-components'
+import { light } from "./styles/Themes"
+
+import Main from './components/Main'
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+
+export default App;
